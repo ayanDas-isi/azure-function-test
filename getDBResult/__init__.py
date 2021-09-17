f46e8c184ede72e2fabe76939ddc6d76159a0024
Ayan Das <ayadas06@in.ibm.com>
2021-09-17 19:37:39 +0530

    added cosmos
    Signed-off-by: Ayan Das <ayadas06@in.ibm.com>

--- getDBResult/__init__.py
+++ getDBResult/__init__.py
@@ -2,23 +2,38 @@ import logging
 
 import azure.functions as func
 
+import pymongo
+import json
+import datetime
+from bson.objectid import ObjectId
+
+
+myclient = pymongo.MongoClient('mongodb://cosmos-acc-ayan:j3VhWD3MQEJ9f2l0hcdEvNBdPKtzYioCDRXFm3oHIyTa6B9ytSQ0JmQ3GT0wf2u1gPPuNRN46jUqeeD2hjuLNQ%3D%3D@cosmos-acc-ayan.mongo.cosmos.azure.com:10255/?authSource=admin&replicaSet=globaldb&maxIdleTimeMS=120000&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=false&ssl=true',
+                               ssl=True)
+mydb = myclient['MFSchema']#
+
+#print(DBRef(collection = "subjects", id = '5ef09c1fcb06406753198e13'))
+#c=mydb['students'].create_index([("studentName", pymongo.ASCENDING),
+#                                    ("className", pymongo.DESCENDING)] ,unique=True)
+# =============================================================================
+# a=mydb['students.subjects'].find({'subject.$sub1':'CS'})
+# for b in a :
+#     print(b)
+# =============================================================================
+
+def pull_data(aggList,tab):
+    a=mydb[tab].aggregate(aggList)
+    return list(a)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse("data pulled from db")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully.111 Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    
+    req_body = req.get_json()
+    tab = req_body.get('tabName')
+    agg=req_body.get('agg')
+    resp=pull_data(agg,tab)
+    return func.HttpResponse(
+        json.dumps(resp, default=str),
+        mimetype="application/json",
+    )
