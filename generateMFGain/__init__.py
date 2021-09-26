417d2c119286b2ca938be282ad7991c72d6d53c0
Ayan Das <ayadas06@in.ibm.com>
2021-09-26 19:57:24 +0530

    traceback
    Signed-off-by: Ayan Das <ayadas06@in.ibm.com>

--- generateMFGain/__init__.py
+++ generateMFGain/__init__.py
@@ -2,11 +2,12 @@ import logging
 
 import azure.functions as func
 from . import mutualFund
-
+import traceback
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    mutualFund.evaluate()
-
+    try:
+        mutualFund.evaluate()
+    except:
+        return func.HttpResponse(str(traceback.format_exc()))
     return func.HttpResponse("Hello,  This HTTP triggered function executed successfully.")
